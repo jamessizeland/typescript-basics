d8b8da9cd48338c513244771509cbdff0f3cc725
James Sizeland <jamessizeland@gmail.com>
2021-04-30 10:51:47 +0100

    adds object type definitions

--- index.ts
+++ index.ts
@@ -9,6 +9,29 @@ let myNumber: number = 10;
 
 let myRegex: RegExp = /foo/;
 
+// Array typing
+
 const names: string[] = userName.split(" ");
 
 const myValues: Array<number> = [1,2,3];
+
+// Object typing
+
+// type definitions can be in one place like this
+interface Person {
+    first: string;
+    last: string;
+}
+
+const myPerson: Person = {
+    first: "James",
+    last: "Sizeland"
+}
+
+// define the key type of an object
+const ids: Record<number, string> = {
+    10: "a",
+    20: "b"
+}
+
+ids[30] = "c";
